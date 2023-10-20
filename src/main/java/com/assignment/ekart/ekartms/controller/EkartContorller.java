a3bdc68bce5a9dd778fb615efe209884e5c14823
unknown <2022mt93183@wilp.bits-pilani.ac.in>
2023-10-20 12:35:36 +0530

    controller modify

--- src/main/java/com/assignment/ekart/ekartms/controller/EkartContorller.java
+++ src/main/java/com/assignment/ekart/ekartms/controller/EkartContorller.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -27,9 +26,6 @@ public class EkartContorller {
 
     @Autowired
     private CustomerCartService customerCartService;
-
-    @Autowired
-    private Environment environment;
 
     @Autowired
     private RestTemplate template;
